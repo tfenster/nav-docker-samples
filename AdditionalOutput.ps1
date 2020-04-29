a6c6b53877eb1dfcffdfeabb0e535db96131e7e1
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-04-29 22:51:22 +0200

    no need

--- AdditionalOutput.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host "Send mail to $env:owner"
-Write-Host "Admin Username      : $username"
-Write-Host ("Admin Password      : "+[System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)))
