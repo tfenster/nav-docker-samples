c1b2c31d3e866b672fd5b05956add3da69c29911
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-05-05 18:34:57 +0200

    fix typo

--- navstart.ps1
+++ navstart.ps1
@@ -1,8 +1,8 @@
 $alops_docker_username = "$env:alops_docker_username"
 $alops_docker_password = "$env:alops_docker_password"
 
-Invoke-Expressione "net user /add $alops_docker_username $alops_docker_password"
-Invoke-Expressione "net localgroup Administrators $alops_docker_username /add"
+Invoke-Expression "net user /add $alops_docker_username $alops_docker_password"
+Invoke-Expression "net localgroup Administrators $alops_docker_username /add"
 $cert = New-SelfSignedCertificate -DnsName "dontcare" -CertStoreLocation Cert:\LocalMachine\My
 winrm create winrm/config/Listener?Address=*+Transport=HTTPS ('@{Hostname="dontcare"; CertificateThumbprint="' + $cert.Thumbprint + '"}')
 winrm set winrm/config/service/Auth '@{Basic="true"}'
