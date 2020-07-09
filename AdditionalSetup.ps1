70180a7436d114e9f6b280b57a1db4bf0a95838d
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-07-10 00:49:20 +0200

    check folder, import cmdlets

--- AdditionalSetup.ps1
+++ AdditionalSetup.ps1
@@ -1,8 +1,11 @@
-Get-ChildItem -Path c:\Run\Apps -Filter *.app | ForEach-Object { 
-    Write-Host "Publishing $($_.FullName)"
-    Publish-NAVApp -ServerInstance $serverInstance -Path "$($_.FullName)"
-    Sync-NavApp -ServerInstance $serverInstance -Name "$($_.Name.Substring(0, $_.Name.Length - 4))"
-    Install-NavApp -ServerInstance $serverInstance -Name "$($_.Name.Substring(0, $_.Name.Length - 4))"
+if (Test-Path C:\Run\Apps -PathType Container) {
+    . c:\run\prompt.ps1
+    Get-ChildItem -Path c:\Run\Apps -Filter *.app | ForEach-Object { 
+        Write-Host "Publishing $($_.FullName) to server instance $serviceName"
+        Publish-NAVApp -ServerInstance $serverInstance -Path "$($_.FullName)"
+        Sync-NavApp -ServerInstance $serverInstance -Name "$($_.Name.Substring(0, $_.Name.Length - 4))"
+        Install-NavApp -ServerInstance $serverInstance -Name "$($_.Name.Substring(0, $_.Name.Length - 4))"
+    }
 }
 
 . (Join-Path $runPath $MyInvocation.MyCommand.Name)
