367cfc54ae93be9ec5afb12cbe357d7f381471c6
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-07-10 00:57:31 +0200

    get instance

--- AdditionalSetup.ps1
+++ AdditionalSetup.ps1
@@ -1,7 +1,9 @@
 if (Test-Path C:\Run\Apps -PathType Container) {
     . c:\run\prompt.ps1
+    $fullInstance = (Get-NAVServerInstance).ServerInstance
+    $serverInstance = $fullInstance.Substring($fullInstance.IndexOf('$') + 1)
     Get-ChildItem -Path c:\Run\Apps -Filter *.app | ForEach-Object { 
-        Write-Host "Publishing $($_.FullName) to server instance $serviceName"
+        Write-Host "Publishing $($_.FullName) to server instance $serverInstance"
         Publish-NAVApp -ServerInstance $serverInstance -Path "$($_.FullName)"
         Sync-NavApp -ServerInstance $serverInstance -Name "$($_.Name.Substring(0, $_.Name.Length - 4))"
         Install-NavApp -ServerInstance $serverInstance -Name "$($_.Name.Substring(0, $_.Name.Length - 4))"
