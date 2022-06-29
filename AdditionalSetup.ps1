1504b3a31c255a150ee168fb439def483e914a26
Tobias Fenster <tfenster@4psbau.de>
2022-06-29 21:27:11 +0200

    add ssl workaround

--- AdditionalSetup.ps1
+++ AdditionalSetup.ps1
@@ -1,6 +1,26 @@
 # invoke default
 . (Join-Path $runPath $MyInvocation.MyCommand.Name)
 
+if (-not("dummy" -as [type])) {
+    add-type -TypeDefinition @"
+using System;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+public static class Dummy {
+    public static bool ReturnTrue(object sender,
+        X509Certificate certificate,
+        X509Chain chain,
+        SslPolicyErrors sslPolicyErrors) { return true; }
+    public static RemoteCertificateValidationCallback GetDelegate() {
+        return new RemoteCertificateValidationCallback(Dummy.ReturnTrue);
+    }
+}
+"@
+}
+
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = [dummy]::GetDelegate()
+
 # heavily inspired and with usage of https://github.com/waldo1001/BusinessCentralOpenAPIToolkit
 #try {
     # setup Waldo's Open API toolkit
