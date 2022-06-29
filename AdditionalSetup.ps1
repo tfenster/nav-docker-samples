6ded9d1da4f0a212aa7e2688f22f794e05c631df
Tobias Fenster <tfenster@4psbau.de>
2022-06-29 16:51:20 +0200

    resilience, start express in background

--- AdditionalSetup.ps1
+++ AdditionalSetup.ps1
@@ -2,34 +2,33 @@
 . (Join-Path $runPath $MyInvocation.MyCommand.Name)
 
 # heavily inspired and with usage of https://github.com/waldo1001/BusinessCentralOpenAPIToolkit
-# setup Waldo's Open API toolkit
-mkdir c:\openapi
-cd c:\openapi
-Invoke-WebRequest -Uri 'https://github.com/waldo1001/BusinessCentralOpenAPIToolkit/archive/refs/heads/main.zip' -UseBasicParsing -OutFile BusinessCentralOpenAPIToolkit.zip
-Expand-Archive .\BusinessCentralOpenAPIToolkit.zip -DestinationPath .
-cd .\BusinessCentralOpenAPIToolkit-main\
-remove-item -Recurse node_modules
-cd .\Microsoft.OpenApi.OData\
-Invoke-WebRequest -Uri 'https://www.nuget.org/api/v2/package/Microsoft.OpenApi.OData/1.0.8' -UseBasicParsing -OutFile Microsoft.OpenApi.OData.zip
-Expand-Archive .\Microsoft.OpenApi.OData.zip -DestinationPath .
-Move-Item .\lib\netstandard2.0\* . -Force
-cd ..
-
-# install choco, nodejs and express
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-refreshenv
-choco feature enable -n allowGlobalConfirmation
-choco install nodejs-lts
-refreshenv
-& 'C:\Program Files\nodejs\npm' install express swagger-ui-express yamljs
-
-# download data model
-$cred = New-Object System.Management.Automation.PSCredential($username, $securepassword)
-Write-Host "user: $username, secpass: $securepassword, cred: $cred"
-$BaseUrl = 'http://localhost:7048/BC/api/v2.0'
-$Outfile = './MicrosoftAPIv2.0/MicrosoftAPIv2.0.edmx'
-$Metadataurl = $BaseUrl + '/$metadata?$schemaversion=2.0&tenant=default'
 try {
+    # setup Waldo's Open API toolkit
+    mkdir c:\openapi
+    cd c:\openapi
+    Invoke-WebRequest -Uri 'https://github.com/waldo1001/BusinessCentralOpenAPIToolkit/archive/refs/heads/main.zip' -UseBasicParsing -OutFile BusinessCentralOpenAPIToolkit.zip
+    Expand-Archive .\BusinessCentralOpenAPIToolkit.zip -DestinationPath .
+    cd .\BusinessCentralOpenAPIToolkit-main\
+    remove-item -Recurse node_modules
+    cd .\Microsoft.OpenApi.OData\
+    Invoke-WebRequest -Uri 'https://www.nuget.org/api/v2/package/Microsoft.OpenApi.OData/1.0.8' -UseBasicParsing -OutFile Microsoft.OpenApi.OData.zip
+    Expand-Archive .\Microsoft.OpenApi.OData.zip -DestinationPath .
+    Move-Item .\lib\netstandard2.0\* . -Force
+    cd ..
+
+    # install choco, nodejs and express
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    refreshenv
+    choco feature enable -n allowGlobalConfirmation
+    choco install nodejs-lts
+    refreshenv
+    & 'C:\Program Files\nodejs\npm' install express swagger-ui-express yamljs
+
+    # download data model
+    $cred = New-Object System.Management.Automation.PSCredential($username, $securepassword)
+    $BaseUrl = 'http://localhost:7048/BC/api/v2.0'
+    $Outfile = './MicrosoftAPIv2.0/MicrosoftAPIv2.0.edmx'
+    $Metadataurl = $BaseUrl + '/$metadata?$schemaversion=2.0&tenant=default'
     Invoke-WebRequest -credential $cred -Uri $Metadataurl -OutFile $Outfile -UseBasicParsing
 
     # convert data model
@@ -39,7 +38,7 @@ try {
     ./Microsoft.OpenApi.OData/OoasUtil.exe -y -i $Source.FullName -o $Dest 
 
     # start express with SwaggerUI frontend 
-    & 'C:\Program Files\nodejs\node' .\MicrosoftAPIv2.0\MicrosoftAPIv2.0.js
+    start-job -ScriptBlock { cd 'C:\openapi\BusinessCentralOpenAPIToolkit-main'; & 'C:\Program Files\nodejs\node' .\MicrosoftAPIv2.0\MicrosoftAPIv2.0.js }
 }
 catch {
     "An error occurred that could not be resolved."
